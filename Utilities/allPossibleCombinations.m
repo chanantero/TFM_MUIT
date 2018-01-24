0e04cf342cf3a0b6f73682a5e4216463d01c1bdd
Rubén Chuliá <rubchume@teleco.upv.es>
2018-01-24 21:10:11 +0100

    Has aprendido que la función ind2sub tiene también la utilidad de encontrar todas las combinaciones de parámetros, cuando esos parámetros pueden tomar un conjunto de valores finitos.

--- Utilities/allPossibleCombinations.m
+++ Utilities/allPossibleCombinations.m
@@ -1,29 +1,41 @@
+params = {'SourceFilter', 'AcousticPath', 'Constraint', 'Grouping', 'ZerosFixed'};
+
 sourceFilter = {'None', 'Loudspeaker'};
 acPath = {'Current', 'Theoric'};
 constraint = {'None', 'Magnitude'};
 grouping = {'Independent', 'Alltogether'};
 zerosFixed = {'No', 'Yes'};
 
-params = {sourceFilter, acPath, constraint, grouping, zerosFixed};
-lengths = cellfun(@(p) numel(p) , params);
+paramValues = {sourceFilter, acPath, constraint, grouping, zerosFixed};
+lengths = cellfun(@(p) numel(p) , paramValues);
 
-% Create all possible combinations matrix
 numCombs = prod(lengths);
 numCol = numel(lengths);
-combs = zeros(numCombs, numCol);
-for k = 1:numCol
-    numCombsAfter = prod(lengths(k+1:end));
-    a = repmat(1:lengths(k), numCombsAfter, 1);
-    a = a(:);
-    numCombsBefore = prod(lengths(1:k-1));
-    col = repmat(a, numCombsBefore, 1);
-    combs(:, k) = col;
-end
+
+% Create all possible combinations matrix
+% % Old way
+% combs = zeros(numCombs, numCol);
+% for k = 1:numCol
+%     numCombsAfter = prod(lengths(k+1:end));
+%     a = repmat(1:lengths(k), numCombsAfter, 1);
+%     a = a(:);
+%     numCombsBefore = prod(lengths(1:k-1));
+%     col = repmat(a, numCombsBefore, 1);
+%     combs(:, k) = col;
+% end
+
+% Better way
+combs = cell(numCol, 1);
+[combs{:}] = ind2sub(lengths, 1:numCombs);
+combs = cell2mat(combs)';
 
 combsString = cell(numCombs, numCol);
 for k = 1:numCombs
     comb = combs(k, :);
     for c = 1:numCol
-        combsString(k, c) = params{c}(comb(c));
+        combsString(k, c) = paramValues{c}(comb(c));
     end
 end
+
+combStringCell = mat2cell(combsString, numCombs, ones(numCol, 1));
+T = table(combStringCell{:}, 'VariableNames', params)
