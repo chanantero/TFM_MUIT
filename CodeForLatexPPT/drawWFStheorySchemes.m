ec3d90f49c07d88e2d695af47e21e1a3f757a768
Rubén Chuliá <rubchume@teleco.upv.es>
2018-07-28 20:53:14 +0200

    Creado el esquema de Rayleigh 2.5D

--- CodeForLatexPPT/drawWFStheorySchemes.m
+++ CodeForLatexPPT/drawWFStheorySchemes.m
@@ -28,7 +28,7 @@ zSect = zeros(size(xSect));
 plot3(ax, xSect, ySect, zSect, 'k')
 
 % Extend the tube
-h = 10;
+h = 0.5;
 s = surf(ax, [xSect; xSect], [ySect; ySect], [zSect + h; zSect - h]);
 s.LineStyle = 'none';
 
@@ -111,33 +111,55 @@ t(7) = text(ax, posMidArc2(1), posMidArc2(2) - 0.01, '$\normSecondPropAngleSecti
 t(8) = text(ax, SSpos(1) + longArrow*cosd(normalDir), SSpos(2) + longArrow*sind(normalDir), '$\surfaceNormal$');
 t(9) = text(ax, 0.1, 0.8, '$\wfsPlane$');
 t(10) = text(ax, 0.8, 0.1, '$\sectionTheo$');
+t(11) = text(ax, SSpos(1), SSpos(2)+0.1, '$\wfsPlaneNormal$');
 
-% Adjust positions manually with the GUI
+ax.View = [30, 30];
+
+% Transparency
+s.AlphaDataMapping = 'none';
+s.FaceAlpha = 'flat';
+s.AlphaData = 0.4*ones(2, length(xSect));
+faceNorm = permute(s.FaceNormals, [2 3 1]);
+[camPos] = pointWiseExtend(ax.CameraPosition, faceNorm);
+visibleFaces = dot(faceNorm, camPos, 2) > 1;
+s.AlphaData(1:2, ~visibleFaces) = 0.4;
+s.AlphaData(1:2, visibleFaces) = 0;
+% 
+% % Adjust positions manually with the GUI
+s.Visible = 'off';
+% % Move labels
+% % ...
+s.Visible = 'on';
 
 % % Then, reset the z position to 0
+% camPos = ax.CameraPosition;
 % for k = 1:numel(t)
-%     t(k).Position(3) = 0;
+%     r = t(k).Position - camPos;
+%     % Find intersection of r with the z = 0 plane, and move the tag there
+%     t(k).Position = camPos - r*camPos(3)/r(3);
 % end
+
+% Hard copy the positions for future versions
+% pos = cell2mat({t.Position}');
+% pos =[
+%   -19.7930   34.6156  -23.0318
+%   -19.6122   34.8834  -22.7867
+%   -19.1841   34.9730  -23.0230
+%    -1.3628    3.1518   -1.8463
+%    -1.1740    3.2675   -1.8362
+%   -19.4345   34.9935  -22.7754
+%    -1.1438    3.3399   -1.7537
+%     0.4001    0.7603         0
+%   -19.6453   34.9192  -22.7044
+%   -19.2172   34.8731  -23.1443
+%   -19.4883   35.0161  -22.6950];
+% ax.View = [30, 30];
 
 % % In the GUI, edit the text but without modify it just to "remind" matlab
 % % that they are text boxes. I don't know why, but if you don't do it, it
 % % doesn't work.
 
-% % Transparency
-% s.AlphaDataMapping = 'none';
-% s.FaceAlpha = 'flat';
-% s.AlphaData = 0.4*ones(2, length(xSect));
-% faceNorm = permute(s.FaceNormals, [2 3 1]);
-% % ax.View = [-20, 55];
-% [camPos] = pointWiseExtend(ax.CameraPosition, faceNorm);
-% visibleFaces = dot(faceNorm, camPos, 2) > 1;
-% s.AlphaData(1:2, ~visibleFaces) = 0.4;
-% s.AlphaData(1:2, visibleFaces) = 0;
-% 
-% currentFolder = pwd;
-% cd(imagesPath); % Needed for inkscape to link svg files properly
-% Plot2LaTeX( ax.Parent, 'pruebaTheoScheme'); % 'Kirchoff2_5DTheoScheme'
-% cd(currentFolder)
+% Plot2LaTeX( ax.Parent, [imagesPath, 'Kirchoff2_5DTheoScheme']); % 'Kirchoff2_5DTheoScheme' pruebaTheoScheme
 
 
 %% Kirchhoff Integral
@@ -295,3 +317,106 @@ ax.Visible = 'off';
 % cd(imagesPath); % Needed for inkscape to link svg files properly
 % Plot2LaTeX( ax.Parent, 'pruebaTheoScheme'); % 'RayleighTheoScheme'
 % cd(currentFolder)
+
+%% Rayleigh 2.5D scenario
+
+ax = axes(figure, 'NextPlot', 'Add', 'DataAspectRatio', [1 1 1]);
+ax.Visible = 'off';
+
+% Create WFS plane
+x = [0 1 1 0 0];
+y = [0 0 1 1 0];
+z = zeros(size(x));
+plot3(ax, x, y, z, 'k')
+
+% Create section of the plane S as an line
+xSectPos = 0.5;
+xSect = [xSectPos xSectPos];
+ySect = [0, 1];
+plot3(ax, xSect, ySect, [0 0], 'k')
+
+% Extend the tube
+h = 0.5;
+s = surf(ax, [xSect; xSect], [ySect; ySect], [h h; -h, -h]);
+s.LineStyle = 'none';
+
+% Lighting properties
+s.FaceLighting = 'gouraud';
+s.BackFaceLighting = 'lit';
+s.FaceNormals = -s.FaceNormals;
+l = light(ax, 'Position', [-1 0 0], 'Style', 'local');
+
+% Create primary source
+PSpos = [0.1 0.3 0];
+ps = scatter3(ax, PSpos(1), PSpos(2), PSpos(3), 50, [0 0 0], 'filled');
+
+% Create receiving point
+RecPos = [0.9 0.7 0];
+recP = scatter3(ax, RecPos(1), RecPos(2), RecPos(3), 50, [0 0 0], 'filled');
+
+% Create vector to mark the distance between the source position and the
+% section line
+plot3(ax, [PSpos(1), xSectPos], [PSpos(2), PSpos(2)], [0, 0], 'k');
+drawbrace([xSectPos, PSpos(2)], [PSpos(1), PSpos(2)], 10); % Add curly brace
+
+% Create vector to mark the distance between the section line and the
+% receiver point
+plot3(ax, [RecPos(1), xSectPos], [RecPos(2), RecPos(2)], [0, 0], 'k');
+drawbrace([xSectPos, RecPos(2)], [RecPos(1), RecPos(2)], 10); % Add curly brace
+
+% Create the line or correct amplitude synthesis
+plot3(ax, [RecPos(1), RecPos(1)], ySect, [0 0], '--k')
+
+% Create the line between source and receiver
+plot3(ax, [RecPos(1), PSpos(1)], [RecPos(2), PSpos(2)], [RecPos(3), PSpos(3)], 'k');
+
+% Highlight the intersection point
+intersectPointY = PSpos(2) + (RecPos(2) - PSpos(2))*(xSectPos - PSpos(1))/(RecPos(1) - PSpos(1));
+scatter3(ax, xSectPos, intersectPointY, 0)
+
+% Add latex labels
+t = gobjects(1);
+offsetX = 0.02; offsetY = -0.02;
+t(1) = text(ax, PSpos(1) + offsetX, PSpos(2) + offsetY, '$\PosTheo[primarySource]$');
+t(2) = text(ax, RecPos(1) + offsetX, RecPos(2) + offsetY, '$\PosTheo$');
+t(3) = text(ax, xSectPos + offsetX, intersectPointY + offsetY, 'Main contribution');
+t(4) = text(ax, (xSectPos + RecPos(1))/2 + offsetX, RecPos(2) + offsetY, '$d$');
+t(5) = text(ax, (xSectPos + PSpos(1))/2 + offsetX, PSpos(2) + offsetY, '$d_{\PosTheoSubInd[primarySource]}$');
+t(6) = text(ax, 0.1, 0.8, '$\wfsPlane$');
+t(7) = text(ax, 0.8, 0.1, '$\sectionTheo$');
+
+% ax.View = [30, 30];
+
+% Transparency
+s.AlphaDataMapping = 'none';
+s.FaceAlpha = 'flat';
+s.AlphaData = 0.4*ones(2, length(xSect));
+faceNorm = permute(s.FaceNormals, [2 3 1]);
+[camPos] = pointWiseExtend(ax.CameraPosition, faceNorm);
+visibleFaces = dot(faceNorm, camPos, 2) > 1;
+s.AlphaData(1:2, ~visibleFaces) = 0.4;
+s.AlphaData(1:2, visibleFaces) = 0;
+
+% % Adjust positions manually with the GUI
+% s.Visible = 'off';
+% % % Move labels
+% % % ...
+% s.Visible = 'on';
+
+% % Then, reset the z position to 0
+% camPos = ax.CameraPosition;
+% for k = 1:numel(t)
+%     r = t(k).Position - camPos;
+%     % Find intersection of r with the z = 0 plane, and move the tag there
+%     t(k).Position = camPos - r*camPos(3)/r(3);
+% end
+
+% Hard copy the positions for future versions
+% pos = cell2mat({t.Position}');
+% ax.View = [30, 30];
+
+% % In the GUI, edit the text but without modify it just to "remind" matlab
+% % that they are text boxes. I don't know why, but if you don't do it, it
+% % doesn't work.
+
+% Plot2LaTeX( ax.Parent, [imagesPath, 'Rayleigh2_5DTheoScheme']); % 'pruebaTheoScheme'
